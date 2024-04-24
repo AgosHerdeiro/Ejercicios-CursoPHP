76d7df4904c7356d3f7ea6256fe3dfb028af184f
Agostina <aherdeiro@alumno.unlam.edu.ar>
2024-04-24 15:25:03 -0300

    arreglo links de assets

--- blogigniter/application/views/admin/post/list.php
+++ blogigniter/application/views/admin/post/list.php
@@ -1,242 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Pace</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php echo APP_NAME . ' | ' . APP_DESCRIPTION ?></title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<!-- Bootstrap 3.3.7 -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/fontawesome-free/css/fontawesome.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/admin/AdminLTE.min.css">
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/admin/custom.css">
+	<!-- AdminLTE Skins. Choose a skin from the css/skins
+		 folder instead of downloading all of them to reduce the load. -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/css/admin/skins/_all-skins.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/fontawesome-free/css/all.min.css">
-    <!-- adminlte-->
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/admin/adminlte.min.css">
+	<script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
 </head>
-<body class="hold-transition sidebar-mini pace-primary">
-<!-- Site wrapper -->
+<body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
-    <!-- Navbar -->
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-            </li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link"></a>
-            </li>
-        </ul>
 
+	<?php $this->load->view("admin/template/header"); ?>
+	<?php $this->load->view("admin/template/nav"); ?>
 
-    </nav>
-    <!-- /.navbar -->
+	<!-- Content Wrapper. Contains page content -->
+	<div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+			<h1></h1>
+			<ol class="breadcrumb">
+				<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+				<li><a href="#">Tables</a></li>
+				<li class="active">Simple</li>
+			</ol>
+		</section>
 
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="../../index3.html" class="brand-link">
-            <img src="#" alt="AdminLTE" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
-        </a>
+		<!-- Main content -->
+		<section class="content">
+			<div class="box">
+				<div class="box-header">
 
-        <!-- Sidebar -->
-        <div class="sidebar">
-            <!-- Sidebar user (optional) -->
-            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div class="image">
-                    <img src="#" class="img-circle elevation-2" alt="User ">
-                </div>
-                <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
-                </div>
-            </div>
-
-            <!-- Sidebar Menu -->
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                    data-accordion="false">
-                    <!-- Add icons to the links using the .nav-icon class
-                         with font-awesome or any other icon font library -->
-                    <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
-                            <p>
-                                Dashboard
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="../../index.html" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard v1</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../../index2.html" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Dashboard v2</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fa fa-list"></i>
-                            <p>
-                                Post
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="<?php echo base_url() ?>admin/post_save" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Crear</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?php echo base_url() ?>admin/post_list" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Listar</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fa fa-list"></i>
-                            <p>
-                                Categorías
-                                <i class="right fas fa-angle-left"></i>
-                            </p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="<?php echo base_url() ?>admin/category_save" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Crear</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?php echo base_url() ?>admin/category_list" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
-                                    <p>Listar</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-            <!-- /.sidebar-menu -->
-        </div>
-        <!-- /.sidebar -->
-    </aside>
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#"></a></li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-
-            <!-- Default box -->
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Bordered Table</h3>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                    <table class="table table-bordered">
-                        <thead>
-                        <tr>
-                            <th style="width: 10px">#</th>
-                            <th>Task</th>
-                            <th>Progress</th>
-                            <th style="width: 40px">Label</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1.</td>
-                            <td>Update software</td>
-                            <td>
-                                <div class="progress progress-xs">
-                                    <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-danger">55%</span></td>
-                        </tr>
-                        <tr>
-                            <td>2.</td>
-                            <td>Clean database</td>
-                            <td>
-                                <div class="progress progress-xs">
-                                    <div class="progress-bar bg-warning" style="width: 70%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-warning">70%</span></td>
-                        </tr>
-                        <tr>
-                            <td>3.</td>
-                            <td>Cron job running</td>
-                            <td>
-                                <div class="progress progress-xs progress-striped active">
-                                    <div class="progress-bar bg-primary" style="width: 30%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-primary">30%</span></td>
-                        </tr>
-                        <tr>
-                            <td>4.</td>
-                            <td>Fix and squish bugs</td>
-                            <td>
-                                <div class="progress progress-xs progress-striped active">
-                                    <div class="progress-bar bg-success" style="width: 90%"></div>
-                                </div>
-                            </td>
-                            <td><span class="badge bg-success">90%</span></td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.card-body -->
-
-            </div>
-            <!-- /.card -->
-
-        </section>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-
-    <!-- /.control-sidebar -->
+				</div>
+				<!-- /.box-header -->
+				<div class="box-body">
+					<div class="box">
+						<div class="box-header">
+							<h3 class="box-title">Condensed Full Width Table</h3>
+						</div>
+						<!-- /.box-header -->
+						<div class="box-body no-padding">
+							<table class="table table-condensed">
+								<tbody>
+								<tr>
+									<th style="width: 10px">#</th>
+									<th>Task</th>
+									<th>Progress</th>
+									<th style="width: 40px">Label</th>
+								</tr>
+								<tr>
+									<td>1.</td>
+									<td>Update software</td>
+									<td>
+										<div class="progress progress-xs">
+											<div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+										</div>
+									</td>
+									<td><span class="badge bg-red">55%</span></td>
+								</tr>
+								<tr>
+									<td>2.</td>
+									<td>Clean database</td>
+									<td>
+										<div class="progress progress-xs">
+											<div class="progress-bar progress-bar-yellow" style="width: 70%"></div>
+										</div>
+									</td>
+									<td><span class="badge bg-yellow">70%</span></td>
+								</tr>
+								<tr>
+									<td>3.</td>
+									<td>Cron job running</td>
+									<td>
+										<div class="progress progress-xs progress-striped active">
+											<div class="progress-bar progress-bar-primary" style="width: 30%"></div>
+										</div>
+									</td>
+									<td><span class="badge bg-light-blue">30%</span></td>
+								</tr>
+								<tr>
+									<td>4.</td>
+									<td>Fix and squish bugs</td>
+									<td>
+										<div class="progress progress-xs progress-striped active">
+											<div class="progress-bar progress-bar-success" style="width: 90%"></div>
+										</div>
+									</td>
+									<td><span class="badge bg-green">90%</span></td>
+								</tr>
+								</tbody>
+							</table>
+						</div>
+						<!-- /.box-body -->
+					</div>
+				</div>
+				<!-- /.box-body -->
+			</div>
+		</section>
+		<!-- /.content -->
+	</div>
+	<!-- /.content-wrapper -->
+	<?php $this->load->view("admin/template/footer"); ?>
+	<div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
-
-
-<script src="<?php echo base_url() ?>assets/js/jquery.min.js"></script>
 
 <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url() ?>assets/js/admin/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url() ?>assets/ckeditor/ckeditor.js"></script>
+<script src="<?php echo base_url() ?>assets/js/admin/demo.js"></script>
+<script src="<?php echo base_url() ?>assets/js/admin/main.js"></script>
 
 </body>
 </html>
