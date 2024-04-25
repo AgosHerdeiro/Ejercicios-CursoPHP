7af386d3caed179005953482b3ed2c870621c8e1
Agostina <aherdeiro@alumno.unlam.edu.ar>
2024-04-25 11:07:10 -0300

    insertar y validar form - codeigniter

--- blogigniter/application/controllers/Admin.php
+++ blogigniter/application/controllers/Admin.php
@@ -2,21 +2,65 @@
 
 class Admin extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-        $this->load->helper('url');
-    }
+		$this->load->database();
 
-    public function index()
-    {
-        $this->load->view('admin/test');
-    }
+		$this->load->library('parser');
+		$this->load->library('form_validation');
 
-    public function post_list()
-    {
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->helper('Post_helper');
+
+		$this->load->model('Post');
+	}
+
+	public function index()
+	{
+		$this->load->view('admin/test');
+	}
+
+	public function post_list()
+	{
 //        $this->load->view('admin/post_list');
-		$this->load->view('admin/post/list');
-    }
+		$view["body"] = $this->load->view('admin/post/list', NULL, TRUE);
+		$view["title"] = "Posts";
+
+		$this->parser->parse('admin/template/body', $view);
+	}
+
+	public function post_save()
+	{
+		if ($this->input->server('REQUEST_METHOD') == 'POST') {
+			$this->form_validation->set_rules('title', 'Título', 'required|min_length[10]|max_length[65]');
+			$this->form_validation->set_rules('content', 'Contenido', 'required|min_length[10]');
+			$this->form_validation->set_rules('description', 'Descripción', 'required|max_length[100]');
+			$this->form_validation->set_rules('posted', 'Publicado', 'required');
+
+			if ($this->form_validation->run()) {
+				$save = array(
+//					'content' => $_POST['content'],
+					'title' => $this->input->post('title'),
+					'content' => $this->input->post('content'),
+					'description' => $this->input->post('description'),
+					'posted' => $this->input->post('posted'),
+				);
+
+				$post_id = $this->Post->insert($save);
+			} else {
+				echo validation_errors();
+			}
+
+
+		}
+
+		$data["data_posted"] = posted();
+		$view["body"] = $this->load->view('admin/post/save', $data, TRUE);
+		$view["title"] = "Crear Post";
+
+		$this->parser->parse('admin/template/body', $view);
+	}
 }
---
+++ blogigniter/application/views/admin/post/save.php
@@ -0,0 +1,97 @@
+<?php echo form_open('', 'class="my_form" enctype="multipart/form-data"'); ?>
+
+<div class="form-group">
+	<?php
+	echo form_label('Título', 'title');
+	?>
+	<?php
+	$text_input = array(
+		'name' => 'title',
+		'id' => 'title',
+		'value' => '',
+		'class' => 'form-control input-lg'
+	);
+	echo form_input($text_input);
+	?>
+</div>
+
+<div class="form-group">
+	<?php
+	echo form_label('Url limpia', 'url_clean');
+	?>
+	<?php
+	$text_input = array(
+		'name' => 'url_clean',
+		'id' => 'url_clean',
+		'value' => '',
+		'class' => 'form-control input-lg'
+	);
+	echo form_input($text_input);
+	?>
+</div>
+
+<div class="form-group">
+	<?php
+	echo form_label('Contenido', 'content');
+	?>
+	<?php
+	$text_area = array(
+		'name' => 'content',
+		'id' => 'content',
+		'value' => '',
+		'class' => 'form-control input-lg'
+	);
+	echo form_textarea($text_area);
+	?>
+</div>
+
+<div class="form-group">
+	<?php
+	echo form_label('Descipción', 'description');
+	?>
+	<?php
+	$text_area = array(
+		'name' => 'description',
+		'id' => 'description',
+		'value' => '',
+		'class' => 'form-control input-lg'
+	);
+	echo form_textarea($text_area);
+	?>
+</div>
+
+<div class="form-group">
+	<?php
+	echo form_label('Imagen', 'image');
+	?>
+	<?php
+	$text_input = array(
+		'name' => 'image',
+		'id' => 'image',
+		'value' => '',
+		'type' => 'file',
+		'class' => 'form-control input-lg'
+	);
+	echo form_input($text_input);
+	?>
+</div>
+
+<div class="form-group">
+	<?php
+	echo form_label('Publicado', 'posted');
+	echo form_dropdown('posted', $data_posted, null, 'class="form-control input-lg"');
+	?>
+</div>
+
+<?php echo form_submit('mysubmit', 'Guardar', 'class="btn btn-primary"') ?>
+
+<?php echo form_close() ?>
+</div>
+<!-- /.box-body -->
+</div>
+</div>
+<!-- /.box-body -->
+</div>
+</section>
+<!-- /.content -->
+</div>
